f7ad87293843dbe372603ec4708825aed64526e9
ronie-18 <rounakjana74@gmail.com>
2026-07-31 01:05:05 +0530

    Update versionCode in app.config.js to 4 and add expo-splash-screen dependency in package.json and package-lock.json

--- app.config.js
+++ app.config.js
@@ -39,7 +39,7 @@ module.exports = () => {
         icon: "./Rider_Logo_nearNow.png",
         edgeToEdgeEnabled: true,
         package: "com.nearandnow.rider",
-        versionCode: 3,
+        versionCode: 4,
         config: {
           googleMaps: {
             apiKey: googleMapsApiKey,
